1ac705fd1529a9f1204181ab0b7330d5dcce4f82
0x109l <danielgamingplays34@gmail.com>
2026-04-13 20:22:59 +0100

    [+] upd loader

--- SAB/loader.lua
+++ SAB/loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/0x109l/rscriptsPBLoader/refs/heads/main/SAB/obf.lua"))
+loadstring(game:HttpGet("https://raw.githubusercontent.com/0x109l/rscriptsPBLoader/refs/heads/main/SAB/obf.lua"))()
