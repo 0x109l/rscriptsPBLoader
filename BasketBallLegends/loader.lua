268c57b0714865dde96fcbf31f9377aa8f6836e0
0x109l <danielgamingplays34@gmail.com>
2026-04-14 14:11:26 +0100

    [+] upd: lol

--- BasketBallLegends/loader.lua
+++ BasketBallLegends/loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/0x109l/rscriptsPBLoader/refs/heads/main/BasketBallLegends/obf.lua"))
+loadstring(game:HttpGet("https://raw.githubusercontent.com/0x109l/rscriptsPBLoader/refs/heads/main/BasketBallLegends/obf.lua"))()
